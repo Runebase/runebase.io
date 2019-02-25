c144137ed78954cac70b5a5e206611891575c25d
bobpepers <bob.pepers@gmail.com>
2019-02-26 00:32:58 +0100

    up

--- src/helpers/useragent.js
+++ src/helpers/useragent.js
@@ -2,12 +2,12 @@ exports.os = function(req, res, next) {
   switch(true)
     {
     case !!req.headers['user-agent'].match(/Win64/):
-         res.downloadLink = "https://downloads.runebase.io/runebase-0.15.1-win64-setup.exe";
-         res.downloadName = "runebase-0.15.1-win64-setup.exe";
+         res.downloadLink = "https://downloads.runebase.io/runebase-0.17.1-win64-setup.exe";
+         res.downloadName = "runebase-0.17.1-win64-setup.exe";
          break;
     case !!req.headers['user-agent'].match(/Win32/):
-         res.downloadLink = "https://downloads.runebase.io/runebase-0.15.1-win32-setup.exe";
-         res.downloadName = "runebase-0.15.1-win32-setup.exe";
+         res.downloadLink = "https://downloads.runebase.io/runebase-0.17.1-win32-setup.exe";
+         res.downloadName = "runebase-0.17.1-win32-setup.exe";
          break;
     case !!req.headers['user-agent'].match(/iPad/):
          res.downloadLink = "ipad";
@@ -18,19 +18,19 @@ exports.os = function(req, res, next) {
          res.downloadName = "iphone";
          break;
     case !!req.headers['user-agent'].match(/Osx/):
-         res.downloadLink = "https://downloads.runebase.io/runebase-0.15.1-osx.dmg";
-         res.downloadName = "runebase-0.15.1-osx.dmg";
+         res.downloadLink = "https://downloads.runebase.io/runebase-0.17.1-osx.dmg";
+         res.downloadName = "runebase-0.17.1-osx.dmg";
          break;
     case !!req.headers['user-agent'].match(/Android/):
          res.downloadLink = "https://play.google.com/store/apps/details?id=org.runebase.wallet";
          res.downloadName = "Google Play";
          break;
     case !!req.headers['user-agent'].match(/Linux/):
-         res.downloadLink = "https://downloads.runebase.io/runebase-0.15.1-x86_64-linux-gnu.tar.gz";
+         res.downloadLink = "https://downloads.runebase.io/runebase-0.17.1-x86_64-linux-gnu.tar.gz";
          res.downloadName = "runebase-0.15.1-x86_64-linux-gnu.tar.gz";
          break;
     default:
-         res.downloadLink = "https://downloads.runebase.io/runebase-0.15.1-x86_64-linux-gnu.tar.gz";
+         res.downloadLink = "https://downloads.runebase.io/runebase-0.17.1-x86_64-linux-gnu.tar.gz";
          res.downloadName = "runebase-0.15.1-x86_64-linux-gnu.tar.gz";
     }
   next();
